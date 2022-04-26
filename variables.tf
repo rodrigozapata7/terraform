0299bb3b0ff7a886cb1336131768f1823f408764
rodrigozapata7 <68240305+rodrigozapata7@users.noreply.github.com>
2022-04-25 21:42:40 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type = string
+  default = "diplomado"
+}
+
+variable "location" {
+  type = string
+  default = "eastus2"
+}
+
+variable "grupo" {
+  type = string
+  default = "grupo"
+}
